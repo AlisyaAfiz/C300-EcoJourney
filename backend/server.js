7963561b52f0b6cb5aba05648ebd2f052415f779
AlisyaAfiz <23018913@myrp.edu.sg>
2025-12-29 11:46:38 +0800

    new changes

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,10 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/', (req, res) => {
+    res.redirect('/login.html');
+});
+
 // Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
